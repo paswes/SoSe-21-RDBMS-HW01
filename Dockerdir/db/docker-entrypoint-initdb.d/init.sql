5045f6cd26940b2562f263d6a3ff25b015ead206
paswes <pascal.westmark@gmail.com>
2021-06-01 10:15:10 +0200

    added foreign key to moods table

--- Dockerdir/db/docker-entrypoint-initdb.d/init.sql
+++ Dockerdir/db/docker-entrypoint-initdb.d/init.sql
@@ -1,17 +1,18 @@
-CREATE TABLE USERS (
-    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE users (
+    user_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NULL DEFAULT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (user_id)
 );
 
-CREATE TABLE MOODS (
-    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE moods (
+    mood_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     score TINYINT NULL DEFAULT NULL,
     timestamp TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
+    fk_user_id INT(10) UNSIGNED NOT NULL,
+    PRIMARY KEY (mood_id),
+    CONSTRAINT fk_user_id FOREIGN KEY (fk_user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO USERS (name) VALUES ('John Doe');
+INSERT INTO users (name) VALUES ('John Doe');
 
-
-INSERT INTO MOODS (score) VALUES (7);
+INSERT INTO moods (score, fk_user_id) VALUES (8, 1);
